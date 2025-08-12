6c4c75f2da39e8663ed49028ce074e23bc9d7f59
hidekiinf <hideki@fujioka.inf.br>
2025-08-11 22:34:11 -0300

    Fix old com communication

--- ComDataExchange/Properties/AssemblyInfo.cs
+++ ComDataExchange/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("ComDataExchange")]
+[assembly: AssemblyTitle("Cystat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComDataExchange")]
+[assembly: AssemblyProduct("Cystat")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
